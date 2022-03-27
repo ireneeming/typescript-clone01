bfb974a96c3c27f3c25117f17f792b1bdaae53f6
ireneeming <minzigom@gmail.com>
2022-03-27 18:28:50 +0900

    typescript, eslint, prettier 세팅'
    :

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      project: "./tsconfig.json",
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "prettier/prettier": 0,
+  },
+};
